58a2d37aa9f9f244a2ad6d8831be7571b70e409f
uloma001 <uloma001@gmail.com>
2022-12-19 09:45:58 -0800

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,7 +11,7 @@ void print_rev(char *s)
 {
 	int len = 0;
 
-	while (*s + len, != '\0')
+	while (*(s + len) != '\0')
 	{
 		len++;
 	}
@@ -19,7 +19,7 @@ void print_rev(char *s)
 
 	while (len >= 0)
 	{
-		_putchar(*s + len);
+		_putchar(*(s + len);
 
 		len--;
 	}
